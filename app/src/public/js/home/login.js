8946208bdaf45a7446ace8f3b3b38737921390d8
seacoding1 <somdays01@gmail.com>
2023-06-13 11:06:33 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const id = document.querySelector("#id"),
+pw = document.querySelector("#pw"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value
+    };
+
+    console.log(req);
+
+};

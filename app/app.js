b9f85de9b6449d0269e22a2ad9667bc577e5ecd5
seacoding1 <somdays01@gmail.com>
2023-06-13 11:45:51 +0900

    로그인을 요청하는 fetch 생성

--- app/app.js
+++ app/app.js
@@ -5,14 +5,14 @@ const express = require('express');//express 모듈을 요청
 const app = express(); //express를 실행시키기 위한 변수
 
 //라우팅
-const home = require("./src/routes/home") //현재폴더에서 routes라는 폴더에서 home 폴더에서 js파일을 불러와줘!
+const home = require("./src/routes/home"); //현재폴더에서 routes라는 폴더에서 home 폴더에서 js파일을 불러와줘!
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 //express의 메서드 중에서 static 정적메서드
 //__dirname 는 현재(app.js)파일의 위치를 반환
-app.use(express.static(`${__dirname}/src/public`))
+app.use(express.static(`${__dirname}/src/public`));
 
 //use는 미들웨어를 등록하는 메서드
 app.use("/", home); //루트(/)경로로 들어오면 home으로 보내주기

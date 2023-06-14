edc88e39ee3bb4794b01df134179a034d32e05aa
seacoding1 <somdays01@gmail.com>
2023-06-14 18:10:17 +0900

    userStorage 모델 구현

---
+++ app/src/models/userStorage.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class userStorage {
+    //클래스 자체에서 users에 접근하고자 하면 static을 붙여 정적 변수로 만들어줘야 한다.
+    //은닉화(private) : private 변수로 바꿔주려면 # 붙이기
+    //데이터를 함부로 아무곳에서 사용하지 못하도록 private 변수로 바꿔주기
+    static #users = {
+        id : ["hihi1", "hihi2", "hihi3"],
+        pw : ["1234", "1234", "1234"],
+        name : ["김이박", "아이린", "김민정"]
+    };
+    //은닉화한 변수를 함수로
+    static getUsers(...fields){
+        const users = this.#users;
+        //            ["id", "pw"]       초기값  , 순회하는 값
+        const newUsers = fields.reduce((newUsers, field) =>{
+            //users에 해당하는 키값=filed 이 있는지
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+
+            console.log(newUsers);
+
+            return newUsers;
+        }, {});
+           //초기값 (이게 지정 안 되어 있으면 newUsers가 초기값)
+        return newUsers;
+    }
+}
+
+module.exports = userStorage;
--- app/src/routes/home/home.controller.js
+++ app/src/routes/home/home.controller.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const userStorage = require("../../models/userStorage");
+
 const output = {
     hello : function (req, res){
         res.render('home/index');
@@ -11,10 +13,6 @@ const output = {
 
 }
 
-const users = {
-    id : ["hihi1", "hihi2", "hihi3"],
-    pw : ["1234", "1234", "1234"]
-};
 
 //login API
 const process = {
@@ -22,19 +20,20 @@ const process = {
         const id = req.body.id,
               pw = req.body.pw;
 
+        const users = userStorage.getUsers("id", "pw");
+        const response = {};
+
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.pw[idx] === pw){
-                return res.json({
-                   success : true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
     }
 }
 
